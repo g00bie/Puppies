07177c237ae2c20f9a9ce8633c5ddebe94f802aa
jjones5787 <jjones5787@W10-JPN6NL3>
2023-02-02 09:12:43 -0600

    Add project files.

---
+++ BOO! Project/frmMain.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPuppy = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnPuppy
+        '
+        Me.btnPuppy.Font = New System.Drawing.Font("Cooper Black", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPuppy.Location = New System.Drawing.Point(206, 148)
+        Me.btnPuppy.Name = "btnPuppy"
+        Me.btnPuppy.Size = New System.Drawing.Size(135, 75)
+        Me.btnPuppy.TabIndex = 0
+        Me.btnPuppy.Text = "Press This Button For a Cute Puppy"
+        Me.btnPuppy.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cooper Black", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(329, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "You Just Got Pranked Dummy!"
+        Me.Label1.Visible = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 370)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPuppy)
+        Me.Name = "frmMain"
+        Me.Text = "Get Spooked Dummy"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPuppy As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ BOO! Project/frmMain.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmMain
+    Private Sub btnPuppy_Click(sender As Object, e As EventArgs) Handles btnPuppy.Click
+        Me.BackgroundImage = My.Resources.skelton
+        btnPuppy.Visible = False
+        Label1.Visible = True
+    End Sub
+End Class
